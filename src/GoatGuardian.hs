3f437129440cc71881dfa7750868a67dd939245f
Dennis Gosnell <cdep.illabout@gmail.com>
2018-06-20 10:21:43 +0900

    Use http-reverse-proxy to reverse-proxy requests for us.

--- src/GoatGuardian.hs
+++ src/GoatGuardian.hs
@@ -1,6 +1,25 @@
-module GoatGuardian
-    ( defaultMain
-    ) where
+module GoatGuardian where
+
+import Network.HTTP.Conduit (newManager, tlsManagerSettings)
+import Network.HTTP.ReverseProxy
+import Network.Wai
+import Network.Wai.Handler.Warp
 
 defaultMain :: IO ()
-defaultMain = putStrLn "someFunc"
+defaultMain = run 3000 app
+
+router :: Request -> IO WaiProxyResponse
+router req = do
+  -- TODO: Make sure the X-UserId header is pulled out of the incoming request.
+  pure $ WPRModifiedRequest req (ProxyDest "localhost" 8000)
+
+app :: Application
+app req respF = do
+  manager <- newManager tlsManagerSettings
+  let waiProxySettings =
+        defaultWaiProxySettings
+          { wpsSetIpHeader = SIHFromHeader
+          -- TODO: Change this so that it removes the X-UserId header.
+          , wpsProcessBody = \_ _ -> Nothing
+          }
+  waiProxyToSettings router waiProxySettings manager req respF
