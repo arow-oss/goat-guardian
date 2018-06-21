0e1c4b2d1f0eee81d236c6a1cdf589b75e57db37
Dennis Gosnell <cdep.illabout@gmail.com>
2018-06-21 17:29:12 +0900

    Start working on the twitter login flow.

--- src/GoatGuardian.hs
+++ src/GoatGuardian.hs
@@ -1,29 +1,37 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module GoatGuardian where
 
+import Control.Monad.Catch (try)
 import Control.Monad.IO.Class
 import Control.Monad.Reader (ask)
 import Data.ByteString (ByteString)
-import Data.Text (Text)
-import Network.HTTP.Conduit (Manager, newManager, tlsManagerSettings)
+import Data.Semigroup ((<>))
+import Data.Text (Text, pack)
+import Network.HTTP.Conduit (HttpException, Manager, parseRequest, newManager, tlsManagerSettings)
+import qualified Network.HTTP.Conduit as HTTPClient
 import Network.HTTP.ReverseProxy
-import Network.Wai
+import Network.Wai (Request, Response, ResponseReceived, pathInfo, requestHeaders)
 import Network.Wai.Handler.Warp
 import System.Envy
-import Tonatona (Plug(..), TonaM, readerShared)
+import Tonatona (Plug(..), TonaM, readerConf, readerShared)
 import qualified Tonatona as Tona
 import Tonatona.Logger (TonaLoggerShared(..), logDebug, logInfo, stdoutLogger)
 import qualified Tonatona.Logger as TonaLogger
+import Web.Authenticate.OAuth (getTemporaryCredential)
+import Web.Twitter.Conduit (OAuth(..), twitterOAuth)
 
 data TwitterConfig = TwitterConfig
-  { twitterOAuthUrl :: Text
+  { twitterOAuthCallbackUrl :: ByteString
   , twitterOAuthKey :: ByteString
   , twitterOAuthSecret :: ByteString
   } deriving Show
 
 instance FromEnv TwitterConfig where
-  fromEnv =
+  fromEnv = do
     TwitterConfig
-      <$> envMaybe "GG_TWITTER_OAUTH_URL" .!= "http://localhost:3000"
+      <$> envMaybe "GG_TWITTER_OAUTH_CALLBACK_URL" .!= "http://localhost:3000/twitter/callback"
       <*> env "GG_TWITTER_OAUTH_KEY"
       <*> env "GG_TWITTER_OAUTH_SECRET"
 
@@ -58,20 +66,52 @@ defaultMain =
     liftIO $ run 3000 $ app conf shared
 
 handleTwitterLogin :: Request -> Tona WaiProxyResponse
-handleTwitterLogin = undefined
+handleTwitterLogin req = do
+  -- oauthUrlReq <- readerShared twitterOAuthUrlReq
+  -- TwitterConfig{twitterOAuthKey, twitterOAuthSecret} <- readerConf twitterConfig
+  -- let cred = clientCred $ Token twitterOAuthKey twitterOAuthSecret
+  -- oauthReq <- oauth cred defaultServer oauthUrlReq
+  -- putStrLn $ "oauth req: " <> show oauthReq
+  -- manager <- readerShared httpManager
+  -- res <- httpLbs oauthReq manager
+  -- putStrLn $ "oauth resp: " <> show res
+  -- undefined
+  $(logDebug) $ "handleTwitterLogin, started..."
+  twitConf <- readerConf twitterConfig
+  let tokens =
+        twitterOAuth
+          { oauthConsumerKey = twitterOAuthKey twitConf
+          , oauthConsumerSecret = twitterOAuthSecret twitConf
+          , oauthCallback = Just $ twitterOAuthCallbackUrl twitConf
+          }
+  manager <- readerShared httpManager
+  $(logDebug) $ "handleTwitterLogin, about to run getTemporaryCredential..."
+  eitherCred <- try $ getTemporaryCredential tokens manager
+  $(logDebug) $ "handleTwitterLogin, eitherCred: " <> tshow eitherCred
+  case eitherCred of
+    Left (err :: HttpException) -> do
+      undefined
+    Right (Credential creds) -> do
+      let maybeCredRes = do
+            confirmed <- lookup "oauth_callback_confirmed" creds
+            token <- lookup "oauth_token" creds
+            secret <- lookup "oauth_secret" creds
+            pure (confirmed, token, secret)
+      case maybeCredRes of
+        Nothing -> undefined
+        Just ("true", token, secret) -> do
+        _ -> do
+          undefined
 
--- this example of using the oauthenticated library is from
--- https://github.com/tel/oauthenticated/blob/master/examples/oauth-authenticate.hs
--- main :: IO ()
--- main = do
---   Opts {..} <- parseArgs
---   req <- Client.parseRequest oauthUrl
---   rng <- Crypto.cprgCreate <$> Crypto.createEntropyPool
---   manager <- Client.newManager Client.defaultManagerSettings
---   let cred = OAuth.clientCred $ OAuth.Token oauthKey oauthSecret
---       app = App manager OAuth.defaultServer cred
---   _ :: Value <- fst <$> evalRWST (makeRequest =<< signRequest req) app rng
--- 	pure ()
+     -- cred <- OA.getTemporaryCredential tokens mgr
+     -- case lookup "oauth_token" $ unCredential cred of
+     --     Just temporaryToken -> do
+     --         liftIO $ storeCredential temporaryToken cred usersToken
+     --         let url = OA.authorizeUrl tokens cred
+     --         redirect $ LT.pack url
+     --     Nothing -> do
+     --         status HT.status500
+     --         text "Failed to obtain the temporary token."
 
 handleProxy :: Request -> Tona WaiProxyResponse
 handleProxy req = do
@@ -102,3 +142,6 @@ app conf shared req respF = do
     (httpManager shared)
     req
     respF
+
+tshow :: Show a => a -> Text
+tshow = pack . show
